13ce552462a0bdd539548d42f6747f9c94af8a53
yefferson <steelchavez53@gmail.com>
2024-04-06 18:36:02 -0500

    today's work 06/04/2024

--- js/script.js
+++ js/script.js
@@ -125,7 +125,7 @@ jQuery(function ($) {
 				slidesToScroll: 1,
 				autoplay: true,
 				dots: true,
-				speed: 600,
+				Speed: 2500, 
 				arrows: true,
 				prevArrow: '<button type="button" class="carousel-control left" aria-label="carousel-control"><i class="fas fa-chevron-left"></i></button>',
 				nextArrow: '<button type="button" class="carousel-control right" aria-label="carousel-control"><i class="fas fa-chevron-right"></i></button>'
